4e7e3ae97aa8a2e6d839cb409dd115d4a82beb9e
Tara Hartlage <rshjeh@gmail.com>
2019-07-25 23:19:42 -0400

    picture slideshow and readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# codelouisville
-Code Louisville Project
+# churchsite
+Church Site
+
+Responsive navigation from mobile to larger screen
+Javascript is shown in the function used on the Contact Us form and the picture slideshow on the Ministries Page. 
+
+I didn't get to building out the Events page yet. 
 
--- script.js
+++ script.js
@@ -25,3 +25,28 @@ function submitForm (e) {
 
 
 
+var image = new Array("images/childsummer3.jpg","images/children1.jpg","images/childfall2.jpg","images/smallgroup1.jpg","images/volunteer1.jpg");
+var imgNum = 0;
+var imgLength = image.length - 1;
+
+function switch_image(num) {
+
+    imgNum = imgNum + num;
+
+    if (imgNum > imgLength) {
+        imgNum = 0;
+    } 
+
+    if (imgNum < 0) {
+        imgNum = imgLength;
+    }
+
+    document.slide.src= image[imgNum];
+
+    return false;
+}
+
+
+
+
+
